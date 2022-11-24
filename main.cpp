5c65901ed78c94431dc1a4417f8b359659786cc0
vadim65060 <vadim65060@users.noreply.github.com>
2022-11-25 00:24:00 +0300

    add tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include "Stack.h"
+#include "Queue.h"
+
+void TestStack() {
+    Stack<int> st(1);
+    st.Push(2);
+    st.Push(3);
+    st.Push(4);
+    st.Push(5);
+    Stack<int> a(st);
+    std::cout<<st;
+    std::cout<<a;
+}
+
+void TestQueue() {
+    Queue<int> queue(1);
+    queue.Push(2);
+    std::cout<<queue;
+    queue.Pop();
+    std::cout<<queue;
+    Queue<int> q2(queue);
+    q2.Pop();
+    std::cout << "q2 empty: " << q2.IsEmpty();
+}
 
 int main() {
-    Stack<int>stack;
-    int n;
-    std::cin>>n;
-    for (int i = 0; i < n; ++i) {
-        std::cin>>stack;
-    }
-    std::cout<<stack;
+    TestStack();
+    TestQueue();
+
 }
